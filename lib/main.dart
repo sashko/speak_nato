d9dc6e1a1cd491fc74acb5089695e30eb5f245a2
Oleksandr Kravchuk <open.source@oleksandr-kravchuk.com>
2018-03-04 23:13:44 +0100

    Add support for routing and navigation
    Added support for routing and navigation. Separated main screen.
    
    Signed-off-by: Oleksandr Kravchuk <open.source@oleksandr-kravchuk.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,71 +1,23 @@
+import 'package:speak_nato/screens/main_screen.dart';
+
 import 'package:flutter/material.dart';
 
-import 'nato.dart';
 
 void main() {
   runApp(new NatoApp());
 }
 
-class NatoApp extends StatefulWidget {
-  NatoAppState createState() => new NatoAppState();
-}
-
-class NatoAppState extends State<NatoApp> {
+class NatoApp extends StatelessWidget {
   String _title = "Speak NATO";
-  String _natoText = "";
-
-  void onTextChanged(String str) {
-    setState(() {
-      _natoText = textToNato(str);
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(title: _title,
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text(_title),
-          actions: <Widget> [
-            new IconButton (
-              icon: new Icon(Icons.view_list),
-              onPressed: () {
-                //onListButtonClicked();
-              },
-            ),
-          ]
-        ),
-        body: new Container(
-          padding: const EdgeInsets.fromLTRB(10.0, 20.0, 10.0, 0.0),
-          child: new Column(
-            children: <Widget>[
-              new TextField(
-                autocorrect: false,
-                autofocus: true,
-                decoration: new InputDecoration(
-                  hintText: 'text to NATOnize...',
-                ),
-                textAlign: TextAlign.center,
-                maxLength: 60,
-                onChanged: (String str) {
-                  onTextChanged(str);
-                },
-              ),
-              new Padding(
-                padding: new EdgeInsets.only(top: 120.0),
-              ),
-              new Text(
-                _natoText,
-                style: new TextStyle(
-                  fontSize: 26.0,
-                  fontWeight: FontWeight.bold,
-                ),
-                textAlign: TextAlign.center,
-              ),
-            ]
-          ),
-        )
-      ),
+    return new MaterialApp(
+      title: _title,
+      routes: <String, WidgetBuilder> {
+        '/MainScreen': (BuildContext context) => new MainScreen(),
+      },
+      home: new MainScreen(),
     );
   }
 }
---
+++ lib/screens/main_screen.dart
@@ -0,0 +1,67 @@
+import 'package:speak_nato/nato.dart';
+
+import 'package:flutter/material.dart';
+
+
+class MainScreen extends StatefulWidget {
+  NatoAppState createState() => new NatoAppState();
+}
+
+class NatoAppState extends State<MainScreen> {
+  String _title = "Speak NATO";
+  String _natoText = "";
+
+  void onTextChanged(String str) {
+    setState(() {
+      _natoText = textToNato(str);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(title: _title,
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text(_title),
+          actions: <Widget> [
+            new IconButton (
+              icon: new Icon(Icons.view_list),
+              onPressed: () {
+              },
+            ),
+          ]
+        ),
+        body: new Container(
+          padding: const EdgeInsets.fromLTRB(10.0, 20.0, 10.0, 0.0),
+          child: new Column(
+            children: <Widget> [
+              new TextField(
+                autocorrect: false,
+                autofocus: true,
+                decoration: new InputDecoration(
+                  hintText: 'text to NATOnize...',
+                ),
+                textAlign: TextAlign.center,
+                maxLength: 60,
+                onChanged: (String str) {
+                  onTextChanged(str);
+                },
+              ),
+              new Padding(
+                padding: new EdgeInsets.only(top: 120.0),
+              ),
+              new Text(
+                _natoText,
+                style: new TextStyle(
+                  fontSize: 26.0,
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ]
+          ),
+        )
+      ),
+    );
+  }
+}
