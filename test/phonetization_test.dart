4ef746ec564d19602ccc7437814665e84736040b
Oleksandr Kravchuk <dev@sashko.rv.ua>
2019-10-23 22:20:16 +0200

    UT: fix coding style
    Made sure `flutter format` is happy about the code.
    
    Signed-off-by: Oleksandr Kravchuk <dev@sashko.rv.ua>

--- test/phonetization_test.dart
+++ test/phonetization_test.dart
@@ -5,7 +5,11 @@ import 'package:speak_nato/nato.dart';
 void testSwedishPhonetization() {
   test("test Swedish letters", () {
     var str = phonetizeText("Yxskaftbud, ge vår WC-zonmö IQ-hjälp", "Swedish");
-    expect(str.trim(), "Yngve Xerxes Sigurd Kalle Adam Filip Tore Bertil Urban David Mellanslag Gustav Erik Mellanslag Viktor Åke Rudolf Mellanslag Wilhelm Caesar Zäta Olof Niklas Martin Östen Mellanslag Ivar Qvintus Helge Johan Ärlig Ludvig Petter");
+    expect(
+        str.trim(),
+        "Yngve Xerxes Sigurd Kalle Adam Filip Tore Bertil Urban David Mellanslag " +
+            "Gustav Erik Mellanslag Viktor Åke Rudolf Mellanslag Wilhelm Caesar Zäta Olof Niklas Martin Östen " +
+            "Mellanslag Ivar Qvintus Helge Johan Ärlig Ludvig Petter");
   });
 
   test("test Swedish digits", () {
@@ -16,8 +20,13 @@ void testSwedishPhonetization() {
 
 void testICAOPhonetization() {
   test("test ICAO letters", () {
-    var str = phonetizeText("The quick brown fox jumps over the lazy dog", "ICAO");
-    expect(str.trim(), "Tango Hotel Echo Space Quebec Uniform India Charlie Kilo Space Bravo Romeo Oscar Whiskey November Space Foxtrot Oscar Xray Space Juliett Uniform Mike Papa Sierra Space Oscar Victor Echo Romeo Space Tango Hotel Echo Space Lima Alpha Zulu Yankee Space Delta Oscar Golf");
+    var str =
+        phonetizeText("The quick brown fox jumps over the lazy dog", "ICAO");
+    expect(
+        str.trim(),
+        "Tango Hotel Echo Space Quebec Uniform India Charlie Kilo Space Bravo Romeo Oscar Whiskey November Space " +
+            "Foxtrot Oscar Xray Space Juliett Uniform Mike Papa Sierra Space Oscar Victor Echo Romeo Space " +
+            "Tango Hotel Echo Space Lima Alpha Zulu Yankee Space Delta Oscar Golf");
   });
 
   test("test ICAO digits", () {
@@ -28,20 +37,36 @@ void testICAOPhonetization() {
 
 void testUkrainianPhonetization() {
   test("test Ukrainian letters", () {
-    var str = phonetizeText("Єхидна, ґава, їжак ще й шиплячі плазуни бігцем форсують Янцзи", "Ukrainian");
-    expect(str.trim(), "Євге́н Христи́на І́грек Дмитро́ Ната́лка Андрі́й Пропуск Ґу́дзик Андрі́й Васи́ль Андрі́й Пропуск Їжа́к Жук Андрі́й Кілова́т Пропуск Щу́ка Ене́й Пропуск Йо́сип Пропуск Шу́ра І́грек Павло́ Левко́ Я́ків Чолові́к Іва́н Пропуск Павло́ Левко́ Андрі́й Зено́вій Украї́на Ната́лка І́грек Пропуск Богда́н Іва́н Григо́рій Центр Ене́й Марі́я Пропуск Фе́дір О́льга Рома́н Степа́н Украї́на Ю́рій Тара́с Знак Пропуск Я́ків Ната́лка Центр Зено́вій І́грек");
+    var str = phonetizeText(
+        "Єхидна, ґава, їжак ще й шиплячі плазуни бігцем форсують Янцзи",
+        "Ukrainian");
+    expect(
+        str.trim(),
+        "Євге́н Христи́на І́грек Дмитро́ Ната́лка Андрі́й Пропуск Ґу́дзик Андрі́й Васи́ль Андрі́й Пропуск " +
+            "Їжа́к Жук Андрі́й Кілова́т Пропуск Щу́ка Ене́й Пропуск Йо́сип Пропуск " +
+            "Шу́ра І́грек Павло́ Левко́ Я́ків Чолові́к Іва́н Пропуск " +
+            "Павло́ Левко́ Андрі́й Зено́вій Украї́на Ната́лка І́грек Пропуск " +
+            "Богда́н Іва́н Григо́рій Центр Ене́й Марі́я Пропуск " +
+            "Фе́дір О́льга Рома́н Степа́н Украї́на Ю́рій Тара́с Знак Пропуск Я́ків Ната́лка Центр Зено́вій І́грек");
   });
 
   test("test Ukrainian digits", () {
     var str = phonetizeText("0123456789", "Ukrainian");
-    expect(str.trim(), "Нуль Один Два Три Чотири П'ять Шість Сім Вісім Дев'ять");
+    expect(
+        str.trim(), "Нуль Один Два Три Чотири П'ять Шість Сім Вісім Дев'ять");
   });
 }
 
 void testGreekPhonetization() {
   test("test Greek letters", () {
-    var str = phonetizeText("διαφυλάξτε γενικά τη ζωή σας από βαθειά ψυχικά τραύματα", "Greek");
-    expect(str.trim(), "Δόξα Ίσκιος Αστήρ Φωφώ Ύμνος Λάμα Ξέρξης Τίγρης Ερμής Χώρος Γαλή Ερμής Ναός Ίσκιος Κενό Χώρος Τίγρης Ηρώ Χώρος Ζευς Ωμέγα Χώρος Σοφός Αστήρ Σοφός Χώρος Αστήρ Πέτρος Χώρος Βύρων Αστήρ Θεά Ερμής Ίσκιος Χώρος Ψυχή Ύμνος Χαρά Ίσκιος Κενό Χώρος Τίγρης Ρήγας Αστήρ Μέλι Αστήρ Τίγρης Αστήρ");
+    var str = phonetizeText(
+        "διαφυλάξτε γενικά τη ζωή σας από βαθειά ψυχικά τραύματα", "Greek");
+    expect(
+        str.trim(),
+        "Δόξα Ίσκιος Αστήρ Φωφώ Ύμνος Λάμα Ξέρξης Τίγρης Ερμής Χώρος Γαλή Ερμής Ναός Ίσκιος Κενό Χώρος " +
+            "Τίγρης Ηρώ Χώρος Ζευς Ωμέγα Χώρος Σοφός Αστήρ Σοφός Χώρος Αστήρ Πέτρος Χώρος " +
+            "Βύρων Αστήρ Θεά Ερμής Ίσκιος Χώρος " +
+            "Ψυχή Ύμνος Χαρά Ίσκιος Κενό Χώρος Τίγρης Ρήγας Αστήρ Μέλι Αστήρ Τίγρης Αστήρ");
   });
 
   test("test Greek digits", () {
@@ -50,11 +75,19 @@ void testGreekPhonetization() {
   });
 }
 
-
 void testGermanPhonetization() {
   test("test German letters", () {
-    var str = phonetizeText("Jeder wackere Bayer vertilgt bequem zwo Schpfund Kalbshaxen.", "German");
-    expect(str.trim(), "Julius Emil Dora Emil Richard Leerzeichen Wilhelm Anton Cäsar Kaufmann Emil Richard Emil Leerzeichen Berta Anton Ypsilon Emil Richard Leerzeichen Viktor Emil Richard Theodor Ida Ludwig Gustav Theodor Leerzeichen Berta Emil Quelle Ulrich Emil Martha Leerzeichen Zacharias Wilhelm Otto Leerzeichen Schule Paula Friedrich Ulrich Nordpol Dora Leerzeichen Kaufmann Anton Ludwig Berta Samuel Heinrich Anton Xanthippe Emil Nordpol Punkt");
+    var str = phonetizeText(
+        "Jeder wackere Bayer vertilgt bequem zwo Schpfund Kalbshaxen.",
+        "German");
+    expect(
+        str.trim(),
+        "Julius Emil Dora Emil Richard Leerzeichen Wilhelm Anton Cäsar Kaufmann Emil Richard Emil Leerzeichen " +
+            "Berta Anton Ypsilon Emil Richard Leerzeichen " +
+            "Viktor Emil Richard Theodor Ida Ludwig Gustav Theodor Leerzeichen " +
+            "Berta Emil Quelle Ulrich Emil Martha Leerzeichen Zacharias Wilhelm Otto Leerzeichen " +
+            "Schule Paula Friedrich Ulrich Nordpol Dora Leerzeichen " +
+            "Kaufmann Anton Ludwig Berta Samuel Heinrich Anton Xanthippe Emil Nordpol Punkt");
   });
 
   test("test German digits", () {
