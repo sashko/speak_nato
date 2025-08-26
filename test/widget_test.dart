6d6363be3fc716006a72296a9c9b9b293bba10cc
Oleksandr Kravchuk <open.source@oleksandr-kravchuk.com>
2025-08-26 19:59:14 +0200

    Remove unused imports

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,10 +4,7 @@
 // find child widgets in the widget tree, read text, and verify that the values of widget properties
 // are correct.
 
-import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
 
-import 'package:speak_nato/main.dart';
 
 void main() {
 
